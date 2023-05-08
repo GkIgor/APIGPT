c6c1f761cca0f06d59bfe2ff8e37b7f7de23beb0
GkIgor <igorf0660@gmail.com>
2023-05-08 17:28:29 -0300

    Estrutura do gpt concluida, adcionando parametros de configurações e tratamento de erros.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const { Client } = require('whatsapp-web.js');
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 const client = new Client();
 //OpenAI requires
 const { Configuration, OpenAIApi } = require("openai");
@@ -6,7 +7,7 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const Piii = require('piii');
+const Piii = require("piii");
 
 class Message {
   constructor(texto) {
@@ -16,26 +17,54 @@ class Message {
 
   isValid() {
     // Verificar se a mensagem não está vazia e não contém palavrões
-    return this.texto && this.texto.trim().length > 0 && !this.piii.check(this.texto);
+    return (
+      this.texto && this.texto.trim().length > 0 && !this.piii.check(this.texto)
+    );
   }
 }
 
-
+async function gpt(prompt) {
   const openai = new OpenAIApi(configuration);
-  const response = await openai.createCompletion({
-  model: "gpt-3.5-turbo",
-  prompt: "Say this is a test",
-  temperature: 0.6,
-  max_tokens: 20,
+  const completion = await openai.createCompletion({
+    model: "gpt-3.5-turbo",
+    role: "assistant",
+    prompt: prompt,
+    temperature: 0.6,
+    max_tokens: 100,
   });
+  let response = completion.data.choices[0].message;
+  return response;
+}
 
+client.on("message", async (message) => {
+  let mensagem = new Message(message.body);
 
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
+  if (mensagem.startsWith("$bot")) {
+    if (mensagem.isValid()) {
+
+      let comando = mensagem.texto.split(" ")[1];
+
+      switch (comando) {
+        // Responder com o link da documentação
+        case '/doc':
+          await message.reply(`\n Consulte a documentação completa em https://api-gpt.vercel.app/`);
+      }
+
+      let response = await gpt(mensagem.texto);
+      await message.reply(`GPT:\n${response}`);
+
+    } else {
+      message.reply(`Desculpe, sua mensagem está fora do escopo de resposta.\n Verifique com os administradores do sistema ou consulte a documentação em https://api-gpt.vercel.app/. @5562986268745`);
+    }
+  }
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
 client.initialize();
